de2dccf71d4023bb0e90d8655f31ba74873e1062
Chamod Abeyrathne <chamodabeyrathna@gmail.com>
2024-01-10 01:17:27 +0000

    V4.2 Admin Vehicle Management System Added

--- admin_forms/adminVehicle.Designer.vb
+++ admin_forms/adminVehicle.Designer.vb
@@ -22,16 +22,77 @@ Partial Class adminVehicle
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(adminVehicle))
+        Label3 = New Label()
+        Label1 = New Label()
+        PictureBox1 = New PictureBox()
+        Label2 = New Label()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI Light", 24F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label3.ForeColor = SystemColors.ActiveCaptionText
+        Label3.Location = New Point(25, 29)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(371, 54)
+        Label3.TabIndex = 11
+        Label3.Text = "Vehicle Management"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI Light", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = SystemColors.ActiveCaptionText
+        Label1.Location = New Point(288, 546)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(552, 41)
+        Label1.TabIndex = 12
+        Label1.Text = "This Section is currently under construction"
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(246, 272)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(623, 341)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 13
+        PictureBox1.TabStop = False
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI Light", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label2.ForeColor = SystemColors.ActiveCaptionText
+        Label2.Location = New Point(295, 585)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(195, 28)
+        Label2.TabIndex = 14
+        Label2.Text = "Hope to see you soon"
         ' 
         ' adminVehicle
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(242), CByte(246), CByte(255))
         ClientSize = New Size(1081, 778)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox1)
+        Controls.Add(Label3)
         FormBorderStyle = FormBorderStyle.None
         Name = "adminVehicle"
         Text = "adminVehicle"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label2 As Label
 End Class
